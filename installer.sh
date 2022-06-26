1a26ade94375d2a3ebd8284840afdbd0971bd19f
Bunny7282927 <107234358+Bunny7282927@users.noreply.github.com>
2022-06-26 15:51:10 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -25,3 +25,25 @@ sleep 3
 echo Stopping File Installer Wizard!
 echo Installing Nodejs Depencinces
 sleep 3
+cd panel
+sleep 1 
+npm install
+sleep 2
+echo DONE!
+echo #######################################################
+echo WIZARD IS INSTALLED NOW YOU MAY DO THE NGINX SETUP
+echo #######################################################
+echo THANKS FOR INSTALLING......
+echo JOIN discord.gg/heliactyl for Support regarding DashBoard Erros and help
+echo MADE BY NOTEXISTED#0001
+echo IF THERE IS ANY ERROR IN SCRIPT THEN DM ME
+echo NotExisted is not owner of Heliactyl
+echo NotExisted is the owner of this Installer Script 
+echo ###############################################
+echo INSTALLED STOPPING WIZARD AND CLOSING THE PANEL......
+echo ###############################################
+echo CLOSING IN 5 SECONDS.......
+sleep 5
+echo CLOSED THE TERMINAL
+exit
+
